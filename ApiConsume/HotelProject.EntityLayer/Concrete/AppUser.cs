3c65ecda2e5ef0d15fc886e4342ac7946b506181
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-07-09 08:58:35 +0300

    Added Identity-related migration files Updated `Context.cs`, `AppUser.cs`, and `AppRole.cs` to support Identity Modified `ContextModelSnapshot.cs` for new schema Updated .csproj files (`DataAccessLayer`, `EntityLayer`, `WebApi`, and `WebUI`) to reflect project structure and dependencies

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string City { get; set; }
+    }
+}
