fd239a60c9ea1601a104a33fb96450472865f413
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-07-19 04:58:08 +0300

    Add footer, scripts, subscribe, and team ViewComponents with corresponding partial views Updated Index.cshtml for component integration

---
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+    public class _TeamPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
