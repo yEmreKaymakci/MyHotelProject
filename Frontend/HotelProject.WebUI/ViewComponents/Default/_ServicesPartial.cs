8a4d40a93033b6b55c39eee0c5938431c7537f68
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-07-22 02:02:17 +0300

    Make _ServicesPartial and _TrailerPartial dynamic with backend logic Defined methods in _ServicesPartial ViewComponent and updated related views

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicesPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_ServicesPartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.RoomDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _ServicesPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ServicesPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5297/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
