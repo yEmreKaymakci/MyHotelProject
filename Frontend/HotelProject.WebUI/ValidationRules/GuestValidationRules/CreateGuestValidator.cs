790c35c5b734a712c58d6f922456f1c41dad164a
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-08-18 18:52:46 +0300

    feat(guest): add full guest management with CRUD, validation and admin UI
    - Added GuestService, GuestManager and repository layer (IGuestDal, EfGuestDal)
    - Implemented GuestController in WebAPI
    - Created DTOs: CreateGuestDto, UpdateGuestDto, ResultGuestDto
    - Added FluentValidation for guest creation
    - Implemented Admin views (AddGuest, Index) and controller
    - Updated Sidebar and layout for navigation

---
+++ Frontend/HotelProject.WebUI/ValidationRules/GuestValidationRules/CreateGuestValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using HotelProject.WebUI.Dtos.GuestDto;
+
+namespace HotelProject.WebUI.ValidationRules.GuestValidationRules
+{
+    public class CreateGuestValidator : AbstractValidator<CreateGuestDto>
+    {
+        public CreateGuestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim alanı boş geçilemez");
+            RuleFor(x => x.SurName).NotEmpty().WithMessage("Soyisim alanı boş geçilemez");
+            RuleFor(x => x.City).NotEmpty().WithMessage("Şehir alanı boş geçilemez");
+            RuleFor(x => x.Name).MinimumLength(3).WithMessage("En az 3 karakter olmalıdır").MaximumLength(20).WithMessage("En fazla 20 karakter olmalıdır");
+            RuleFor(x => x.SurName).MinimumLength(2).WithMessage("En az 2 karakter olmalıdır").MaximumLength(30).WithMessage("En fazla 30 karakter olmalıdır");
+            RuleFor(x => x.City).MinimumLength(3).WithMessage("En az 3 karakter olmalıdır").MaximumLength(20).WithMessage("En fazla 20 karakter olmalıdır");
+        }
+    }
+
+}
+
