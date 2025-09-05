bd9d8c0e3c218ba6762066044d2172e27d5b81ed
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-09-06 01:41:39 +0300

    feat: implement MessageCategory feature with full CRUD operations
    - Add MessageCategory entity and related DTOs
    - Implement service and repository layers for MessageCategory
    - Create API controllers for MessageCategory management
    - Add database context and EF configurations
    - Integrate MessageCategory dropdown in Contact form
    - Add migration for MessageCategory table

---
+++ ApiConsume/HotelProject.DataAccessLayer/EntityFramework/EfMessageCategoryDal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.Repositories;
+using HotelProject.EntityLayer.Concrete;
+
+namespace HotelProject.DataAccessLayer.EntityFramework
+{
+    public class EfMessageCategoryDal : GenericRepository<MessageCategory>, IMessageCategoryDal
+    {
+        private readonly Context _context;
+        public EfMessageCategoryDal(Context context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
