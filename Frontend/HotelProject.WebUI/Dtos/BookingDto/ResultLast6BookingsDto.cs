1cf9348b670d063f6b3541ea227c0ed0b6d73d2c
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-10-02 02:15:34 +0300

    feat(dashboard): add booking city/country migration & new dashboard partials
    - Added migration: booking_city_country
    - Updated ContextModelSnapshot and Booking entity
    - Extended BookingDto with ResultLast6BookingsDto
    - Created new Dashboard components:
    - _DashboardScriptPartial
    - _DashboardSubscribeCountPartial
    - Updated Index.cshtml and Default.cshtml for integration

--- Frontend/HotelProject.WebUI/Dtos/BookingDto/ResultLast6BookingsDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/BookingDto/ResultLast6BookingsDto.cs
@@ -13,5 +13,7 @@
         public string SpecialRequest { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
     }
 }
