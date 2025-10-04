e5e3ca741660a7b3ccf361e12f531b608548dd74
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-10-05 01:41:45 +0300

    feat(role-management): add role assign and update features
    - Added RoleAssignController and RoleController for role operations
    - Created AddRoleViewModel and UpdateRoleViewModel
    - Implemented AddRole, UpdateRole, and AssignRole views
    - Integrated Facebook followers DTO into dashboard
    - Updated SidebarPartial and DashboardSubscribeCountPartial for UI improvements

---
+++ Frontend/HotelProject.WebUI/Dtos/FollowersDto/ResultFacebookFollowersDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelProject.WebUI.Dtos.FollowersDto
+{
+    public class ResultFacebookFollowersDto
+    {
+        public Results results { get; set; }
+
+        public class Results
+        {
+            public int likes { get; set; }
+            public int followers { get; set; }
+        }
+
+    }
+}
--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -71,6 +71,34 @@ namespace HotelProject.WebUI.ViewComponents.Dashboard
                 ViewBag.v5 = resultLinkedinFollowersDtos?.data?.follower_count ?? 0;
                 ViewBag.v6 = resultLinkedinFollowersDtos?.data?.connection_count ?? 0;
             }
+
+
+
+
+            var client4 = new HttpClient();
+            var request4 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://facebook-scraper3.p.rapidapi.com/page/details?url=https%3A%2F%2Fwww.facebook.com%2Fkanyewest"),
+                Headers =
+    {
+        { "x-rapidapi-key", "eaa8321078msh8aa48935ef5e1d8p1cb46fjsn6493e1885e8e" },
+        { "x-rapidapi-host", "facebook-scraper3.p.rapidapi.com" },
+    },
+            };
+            using (var response4 = await client4.SendAsync(request4))
+            {
+                response4.EnsureSuccessStatusCode();
+                var body4 = await response4.Content.ReadAsStringAsync();
+                ResultFacebookFollowersDto resultFacebookFollowersDtos = JsonConvert.DeserializeObject<ResultFacebookFollowersDto>(body4);
+
+                ViewBag.v7 = resultFacebookFollowersDtos?.results?.followers ?? 0;
+                ViewBag.v8 = resultFacebookFollowersDtos?.results?.likes ?? 0;
+            }
+
+
+
+
             return View();
         }
     }
